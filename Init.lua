268b9e8a1fa549a78b7e5ad78a99e5b4e3a3b7dc
Secondline <15008966+secondline@users.noreply.github.com>
2026-04-26 10:48:06 +0800

    Initialize HARFDB options and savedIndicators
    Initialize options and savedIndicators in HARFDB if not already set.

--- Init.lua
+++ Init.lua
@@ -8,10 +8,16 @@ NS.Debug = {}
 NS.Version = C_AddOns.GetAddOnMetadata(addonName, 'Version')
 NS.DevEnv = false
 
---Initialize saved variables
 HARFDB = HARFDB or {}
+
+if not HARFDB.options then 
+    HARFDB.options = {} 
+end
+if not HARFDB.savedIndicators then 
+    HARFDB.savedIndicators = {} 
+end
+
 if HARFDB.version ~= NS.Version then
+    HARFDB.options.buffIcons = true
     HARFDB.version = NS.Version
 end
-if not HARFDB.options then HARFDB.options = {} end
-if not HARFDB.savedIndicators then HARFDB.savedIndicators = {} end
